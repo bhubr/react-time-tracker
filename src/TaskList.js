53030c86270c0fa40ef816b5eca7923e0c15e3eb
Benoît Hubert <benoithubert@gmail.com>
2018-08-21 16:48:04 +0200

    Put tasks in an array and map over it

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { List } from 'semantic-ui-react'
 
+const tasks = [
+  {
+    title: 'Créer une appli de gestion de tâches',
+    done: false
+  },
+  {
+    title: 'Faire le tri dans mes dépôts GitHub',
+    done: false
+  },
+  {
+    title: 'Faire le tri dans mes backups',
+    done: false
+  }
+]
+
 const TaskList = () => (
   <List divided verticalAlign='middle'>
-    <List.Item>
-      <List.Content>
-        <List.Header as='a'>Créer une appli de gestion de tâches</List.Header>
-      </List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Content>
-        <List.Header as='a'>Faire le tri dans mes dépôts GitHub</List.Header>
-      </List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Content>
-        <List.Header as='a'>Faire le tri dans mes backups</List.Header>
-      </List.Content>
-    </List.Item>
+    {
+      tasks.map(
+        (task, index) =>
+        <List.Item key={index}>
+          <List.Content>
+            <List.Header as='a'>{task.title}</List.Header>
+          </List.Content>
+        </List.Item>
+      )
+    }
   </List>
 )
 
