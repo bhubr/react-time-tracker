d40312a02f2193a8bb20e7eec8c24ab38c6d580a
Benoît Hubert <benoithubert@gmail.com>
2018-09-25 21:27:19 +0200

    Allow ignoring fields from payload on update

--- back/models/lib/prepareUpdateQuery.js
+++ back/models/lib/prepareUpdateQuery.js
@@ -1,6 +1,9 @@
-module.exports = (table, id, payload, updatableFields) => {
+module.exports = (table, id, payload, updatableFields, ignoredFields = []) => {
   const keys = []
   const values = []
+  for (let field of ignoredFields) {
+    delete payload[field]
+  }
   for (let key in payload) {
     if (! updatableFields.includes(key)) {
       throw new Error(`Task update: ${key} field cannot be updated`)
