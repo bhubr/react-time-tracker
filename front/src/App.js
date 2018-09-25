57a4138f11174f9fb8fb883f00d72629d7adb6a2
Benoît Hubert <benoithubert@gmail.com>
2018-09-25 21:32:07 +0200

    Remove non-redux-based state mutations

--- front/src/App.js
+++ front/src/App.js
@@ -15,48 +15,8 @@ class App extends Component {
     timer: null,
     modalOpen: false
   }
-  startTimeSlice = taskId => {
-    const task = this.state.tasks.find(task => task.id === taskId)
-    if (task.done) {
-      return
-    }
-    const interval = setInterval(this.timerTick, 1000)
-    const timer = {
-      interval,
-      comment: '',
-      remainingTime: TIMER_SLICE_DURATION,
-      datetimeStart: new Date(),
-      taskId: task.id
-    }
-    this.setState({
-      timer,
-      modalOpen: true
-    })
-  }
-  timerTick = () => {
-    this.setState(prevState => {
-      const timer = { ...prevState.timer }
-      timer.remainingTime -= 1
-      if(timer.remainingTime > 0) {
-        return { timer }
-      }
-      notifyMe()
-      clearInterval(timer.interval)
-      const { datetimeStart, taskId, comment } = timer
-      const datetimeEnd = new Date()
-      const tasks = this.state.tasks.map(task => ({ ...task }))
-      const task = tasks.find(task => task.id === taskId)
-      if (!task.timeSlices) {
-        task.timeSlices = []
-      }
-      task.timeSlices.push({
-        datetimeStart: datetimeStart.toString(),
-        datetimeEnd: datetimeEnd.toString(),
-        comment
-      })
-      // storeTasks(tasks)
-      return { tasks, timer: null }
-    })
+  componentDidMount() {
+    document.title = 'Pomodoro'
   }
   onCommentSubmit = comment => {
     const timer = { ...this.state.timer }
