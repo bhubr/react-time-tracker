0f34cc237f7dcd37a2e09bd845e7311df1df829e
Benoît Hubert <benoithubert@gmail.com>
2020-02-24 08:07:02 +0100

    Select tasks to add to daily view aka "To do today"

--- packages/front/src/components/WorkspaceList.jsx
+++ packages/front/src/components/WorkspaceList.jsx
@@ -1,44 +1,58 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'class-names';
 import { fetchWorkspaces as fetchWorkspacesAction } from '../store/workspaces/actions';
 import workspacePropTypes from '../prop-types/workspace';
 
-class WorkspaceList extends React.Component {
-  componentDidMount() {
-    const { fetchWorkspaces } = this.props;
-    fetchWorkspaces();
-  }
+function WorkspaceList({ workspaces, fetchWorkspaces }) {
+  const [selectedTasksIds, setSelectedTasksIds] = useState([]);
 
-  render() {
-    const { workspaces } = this.props;
-    return (
-      <div className="ProjectList">
-        {
-        workspaces.map((workspace) => (
-          <div key={workspace.id}>
-            <h4>{workspace.title}</h4>
-            <ul>
-              {workspace.projects.map((project) => (
-                <li key={`project-${project.id}`}>
-                  {project.title}
-                  <ul>
-                    {project.tasks.map((task) => (
-                      <li key={`task-${task.id}`} className={classNames({ 'task--done': task.done })}>
-                        {task.title}
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))
-      }
-      </div>
-    );
-  }
+  useEffect(() => {
+    fetchWorkspaces();
+  }, []);
+
+  const toggleSelected = (taskId) => {
+    const nextSelectedTasksIds = selectedTasksIds.includes(taskId)
+      ? selectedTasksIds.filter((id) => id !== taskId)
+      : [...selectedTasksIds, taskId];
+    setSelectedTasksIds(nextSelectedTasksIds);
+  };
+
+  return (
+    <div className="ProjectList">
+      {
+      workspaces.map((workspace) => (
+        <div key={workspace.id}>
+          <h4>{workspace.title}</h4>
+          <ul>
+            {workspace.projects.map((project) => (
+              <li key={`project-${project.id}`}>
+                {project.title}
+                <ul>
+                  {project.tasks.map((task) => (
+                    <li
+                      key={`task-${task.id}`}
+                      className={classNames({
+                        'task--done': task.done,
+                        'task--selected': selectedTasksIds.includes(task.id),
+                      })}
+                      onClick={() => toggleSelected(task.id)}
+                    >
+                      {task.title}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))
+    }
+    </div>
+  );
 }
 
 WorkspaceList.propTypes = {
